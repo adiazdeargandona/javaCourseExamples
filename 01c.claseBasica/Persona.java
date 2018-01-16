804404c7a52f2a89cdee21f1c9eb8ffe6918a313
David Contreras <davidcb@comillas.edu>
2018-01-16 12:08:42 +0100

    Ejemplo de clase básico Persona

---
+++ 01c.claseBasica/Persona.java
@@ -0,0 +1,30 @@
+/* Se recomineda que el nombred e la 
+clase identifique el tipo objeto */
+public class Persona
+{
+	//Atributos
+	String nombre;
+	int edad;
+
+	//Metodos
+	String getNombre()
+	{
+		return nombre;
+	}
+
+	int getEdad()
+	{
+		return edad;
+	}
+
+	void setNombre(String x)
+	{
+		nombre = x;
+	}
+
+	void setEdad(int x)
+	{
+		edad = x;
+	}
+}
+

0d8300142c94c50dff242cc4d70dea7a8f0f0c35
David Contreras <davidcb@comillas.edu>
2018-02-02 10:00:39 +0100

    Práctica 02 - Atributos de clase y arrays (update)

--- 02.arrayCuadradosDibujo/Cuadrado.java
+++ 02.arrayCuadradosDibujo/Cuadrado.java
@@ -37,19 +37,21 @@ public class Cuadrado
 	void setX(int x)
 	{
 		if(x>0 && x<800)
-			this.x=x;
+			this.x = x;
 	}
 
 	void setY(int y)
 	{
 		if(y>0 && y<600)
-			this.y=y;
+			this.y = y;
 	}
 
 	void setLado(int lado)
 	{
 		if(x>1 && x<600)
-			this.lado=lado;
+			this.lado = lado;
+		else
+			this.lado = 2;
 	}
 
 	static void setRELLENO(boolean relleno)

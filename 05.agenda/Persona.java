a7ef7b0f12c099dd904b69c05272d384e097e0aa
David Contreras <davidcb@comillas.edu>
2018-02-23 13:10:18 +0100

    Práctica de Agenda de Personas - Solución

--- 05.agenda/Persona.java
+++ 05.agenda/Persona.java
@@ -1,31 +1,84 @@
-/**
-@author David Contreras
-*/
+/** @author David Contreras */
 
 public class Persona
 {
-    int edad;
-    String nombre;
-   
-    public Persona(String nombre, int edad)
+    public final static int EDAD_MAXIMA = 120;
+    public final static int EDAD_MINIMA = 0;
+
+    private String nombre;
+    private int edad;
+    private String nif;
+
+    public Persona(String nombre, String nif, int edad)
     {
         this.nombre = nombre;
-        this.setEdad(edad);
+        this.nif = nif;
+        this.setEdad(edad);     
     }
-   
-    int getEdad()
+
+    public Persona(String nombre, String nif)
+    {
+        this.nombre = nombre;
+        this.nif = nif;
+    }
+
+    /* Contructor de búsqueda: equals */
+    public Persona(String nif)
+    {
+        this.nif = nif;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+
+    public String getNif()
+    {
+        return nif;
+    }
+
+    public int getEdad()
     {
         return edad;
     }
-   
-    void setEdad(int edad)
+
+    public void setNif(String nif)
     {
-        if(edad > 0 && edad < 120)
-            this.edad = edad;
+        this.nif = nif;
     }
-   
-    String getNombre()
+
+    public void setNombre(String nombre)
     {
-        return nombre;
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad)
+    {
+        if(edad > EDAD_MINIMA && edad < EDAD_MAXIMA)
+            this.edad = edad;       
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Nombre: " + nombre + " NIF: " +
+         nif + " Edad: " + edad;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(obj instanceof Persona)
+        {
+            Persona p = (Persona) obj;
+            if (nif.equals(p.getNif()))
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
     }
 }
